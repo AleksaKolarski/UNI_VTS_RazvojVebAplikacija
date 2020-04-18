e9d2c2fe5c44069d2539b4767be9d5fd4817ced3
Aleksa-PC <aleksa.kolarski@yahoo.com>
2020-04-18 13:40:04 +0200

    Fixed LESS loading in prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -16,18 +16,11 @@ module.exports = merge(common, {
         new HtmlWebpackPlugin({
             template: './src/template.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
     ],
     module: {
         rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader, // 3. Extract css into files
-                    "css-loader", // 2. Turns css into commonjs
-                    "sass-loader" // 1. Turns sass into css
-                ]
-            },
             {
                 test: /\.less$/,
                 use: [
